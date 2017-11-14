50584be851d1eb5f9eb99225c392ffe41c239111
AREUM AN <areum.an.0928@gmail.com>
2017-11-14 18:29:00 -0500

    First finish

--- app/src/main/java/work/andreaamado/aagameapp/GameNumbersActivity_fpage.java
+++ app/src/main/java/work/andreaamado/aagameapp/GameNumbersActivity_fpage.java
@@ -7,7 +7,9 @@ import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -23,6 +25,7 @@ public class GameNumbersActivity_fpage extends Fragment {
     List<Quiz> questions = new ArrayList<Quiz>();
 
     TextView lblquestion;       // show quiz
+    TextView lblquestionNum;    // number
 
     int showedQuestion = 0;
 
@@ -38,37 +41,115 @@ public class GameNumbersActivity_fpage extends Fragment {
         View view = inflater.inflate(R.layout.activity_game_numbers_fragment, container, false);
 
         lblquestion = (TextView) view.findViewById(R.id.question);
+        lblquestionNum = (TextView) view.findViewById(R.id.question_number);
 
-//        for(int i=0; i <((GameRandomActivity)getActivity()).question.length; i++) {
-//            System.out.println("random page: " + ((GameRandomActivity)getActivity()).question[i]);
-//        }
+        final Button btn0 = (Button) view.findViewById(R.id.btn_0);
+        final Button btn1 = (Button) view.findViewById(R.id.btn_1);
+        final Button btn2 = (Button) view.findViewById(R.id.btn_2);
+        final Button btn3 = (Button) view.findViewById(R.id.btn_3);
+        final Button btn4 = (Button) view.findViewById(R.id.btn_4);
+        final Button btn5 = (Button) view.findViewById(R.id.btn_5);
+        final Button btn6 = (Button) view.findViewById(R.id.btn_6);
+        final Button btn7 = (Button) view.findViewById(R.id.btn_7);
+        final Button btn8 = (Button) view.findViewById(R.id.btn_8);
+        final Button btn9 = (Button) view.findViewById(R.id.btn_9);
 
-        // Make Quiz 1
-        makeFirstQuiz();
 
-        // Click the ENTER button
-        view.findViewById(R.id.btn_enter).setOnClickListener(new View.OnClickListener() {
+        View.OnClickListener clickListener = new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                // Check out of questions
-                if(showedQuestion < 5) {  makeQuiz(); }
-                else {
+            public void onClick(View v) {
+                //Toast.makeText(v.getContext(),((Button) v).getText(), Toast.LENGTH_LONG).show();
+
+                String text = (String) lblquestionNum.getText();
+                text += ((Button) v).getText();
+
+                if (Integer.parseInt(text) > 0 && Integer.parseInt(text) < 101) {
+                    lblquestionNum.setText(Integer.parseInt(text) + "");
+                } else {
                     AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-                    builder.setTitle("Out of Questions");
-                    builder.setMessage("It's going to Category page ");
+                    //builder.setTitle("Warring");
+                    builder.setMessage("Please, put number between 1 and 100.");
 
                     builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
 
                         public void onClick(DialogInterface dialog, int which) {
-                            goMain();
+                            lblquestionNum.setText("");
                             dialog.dismiss();
                         }
                     });
 
                     AlertDialog alert = builder.create();
                     alert.show();
-
                 }
+
+
+
+            }
+
+        };
+        btn0.setOnClickListener(clickListener);
+        btn1.setOnClickListener(clickListener);
+        btn2.setOnClickListener(clickListener);
+        btn3.setOnClickListener(clickListener);
+        btn4.setOnClickListener(clickListener);
+        btn5.setOnClickListener(clickListener);
+        btn6.setOnClickListener(clickListener);
+        btn7.setOnClickListener(clickListener);
+        btn8.setOnClickListener(clickListener);
+        btn9.setOnClickListener(clickListener);
+
+
+//        for(int i=0; i <((GameRandomActivity)getActivity()).question.length; i++) {
+//            System.out.println("random page: " + ((GameRandomActivity)getActivity()).question[i]);
+//        }
+
+        // Make Quiz 1
+        //makeFirstQuiz();
+        lblquestion.setText("Please, Enter number of Jenga block.");
+
+        // Click the ENTER button
+        view.findViewById(R.id.btn_enter).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String questionNumtxt = (String) lblquestionNum.getText();
+
+                if(!lblquestionNum.getText().equals("")){
+                    // Check out of questions
+                    if(showedQuestion == 0) {makeFirstQuiz(questionNumtxt);}
+                    else if(showedQuestion > 0 && showedQuestion < 5) {  makeQuiz(questionNumtxt); }
+                    else {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                        builder.setTitle("Out of Questions");
+                        builder.setMessage("It's going to Category page ");
+
+                        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+                            public void onClick(DialogInterface dialog, int which) {
+                                goMain();
+                                dialog.dismiss();
+                            }
+                        });
+
+                        AlertDialog alert = builder.create();
+                        alert.show();
+
+                    }
+                } else {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+//                    builder.setTitle("Out of Questions");
+                    builder.setMessage("Please, Enter the number first.");
+
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+
+                    AlertDialog alert = builder.create();
+                    alert.show();
+                }
+
 
                 }
         });
@@ -87,7 +168,7 @@ public class GameNumbersActivity_fpage extends Fragment {
     }
 
     // make quiz1 (with make questions array)
-    public void makeFirstQuiz() {
+    public void makeFirstQuiz(String questionNumtxt) {
         // make questions array and shuffle
         for (int i = 0; i < ((GameNumbersActivity)getActivity()).question.length; i++) {
             questions.add(new Quiz(((GameNumbersActivity)getActivity()).question[i]));
@@ -95,19 +176,21 @@ public class GameNumbersActivity_fpage extends Fragment {
         Collections.shuffle(questions);
 
         // show
-        makeQuiz();
+        makeQuiz(questionNumtxt);
 
     }
 
     // make quiz and answers statements
-    public void makeQuiz() {
+    public void makeQuiz(String questionNumtxt) {
 
         Quiz quiz = questions.get(0);
         // remove used questions element that was used for preventing duplication
         questions.remove(0);
 
         // set number of quiz and question.
-        lblquestion.setText(quiz.question);
+        lblquestion.setText("Q." + questionNumtxt + ": " + quiz.question);
+        //lblquestion.setText("Q." + lblquestionNum + ": " + quiz.question);
+        lblquestionNum.setText("");
 
         // How many question showed
         showedQuestion++;
